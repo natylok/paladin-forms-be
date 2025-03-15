75ba652e5405cf6c2831850c4db66618f937f322
Nati Levy <natilevy@Natis-MacBook-Pro.local>
2025-03-15 22:04:46 +0200

    WIP

--- queue-service/src/queue/queue.module.ts
+++ queue-service/src/queue/queue.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { HttpModule } from '@nestjs/axios';
 import { QueueController } from './queue.controller';
 import { QueueService } from './queue.service';
 
 @Module({
+  imports: [
+    HttpModule.register({
+      timeout: 30000,
+      maxRedirects: 5,
+    })
+  ],
   controllers: [QueueController],
   providers: [QueueService],
   exports: [QueueService]
