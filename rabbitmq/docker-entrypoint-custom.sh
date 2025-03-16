9797f3d8852bf2ff8c2b2b1fef91693c67d8ba10
Nati Levy <natilevy@Natis-MacBook-Pro.local>
2025-03-16 10:08:39 +0200

    WIP

--- rabbitmq/docker-entrypoint-custom.sh
+++ rabbitmq/docker-entrypoint-custom.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 set -e
 
-# Start RabbitMQ in the background
-rabbitmq-server -detached
+# First run the original entrypoint script
+docker-entrypoint.sh rabbitmq-server -detached
 
-# Wait for RabbitMQ to start
-sleep 5
+# Wait for RabbitMQ to be ready
+until rabbitmqctl await_startup; do
+    sleep 2
+done
+
+echo "Configuring RabbitMQ users..."
 
 # Delete default guest user
 rabbitmqctl delete_user guest || true
 
 # Add new user with admin privileges
-rabbitmqctl add_user "$RABBITMQ_DEFAULT_USER" "$RABBITMQ_DEFAULT_PASS"
+rabbitmqctl add_user "$RABBITMQ_DEFAULT_USER" "$RABBITMQ_DEFAULT_PASS" || true
 rabbitmqctl set_user_tags "$RABBITMQ_DEFAULT_USER" administrator
 rabbitmqctl set_permissions -p / "$RABBITMQ_DEFAULT_USER" ".*" ".*" ".*"
 
-# Stop the detached RabbitMQ
-rabbitmqctl stop
+# Stop the background instance
+rabbitmqctl stop_app
 
 # Start RabbitMQ in the foreground
 exec rabbitmq-server 
