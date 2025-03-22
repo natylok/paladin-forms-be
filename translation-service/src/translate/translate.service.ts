a0ea289a0633d5ebdff9fd6ee874956841f63e5c
Nati Levy <natilevy@Natis-MacBook-Pro.local>
2025-03-22 19:32:32 +0400

    WIP

--- translation-service/src/translate/translate.service.ts
+++ translation-service/src/translate/translate.service.ts
@@ -30,33 +30,55 @@ export class TranslateService implements OnModuleInit {
         try {
           // Deep clone components to avoid conflicts between translations
           const originalComponents = JSON.parse(JSON.stringify(survey.components));
+          this.logger.log(`Starting translation for ${surveyId} to ${targetLang}`, {
+            componentCount: originalComponents.length
+          });
           
           // Translate all components and create new translated components
-          const translatedComponents = await Promise.all(originalComponents.map(async (component) => {
-            // Create a new component object for this translation
-            const translatedComponent = { ...component };
-            
-            // Translate title
-            translatedComponent.title = await this.translatorService.translate(
-              component.title,
-              sourceLang,
-              targetLang
-            );
-            
-            // Translate options if they exist
-            if (component.options?.length) {
-              translatedComponent.options = await Promise.all(
-                component.options.map(option =>
-                  this.translatorService.translate(option, sourceLang, targetLang)
-                )
-              );
-            }
-            
-            return translatedComponent;
-          }));
+          const translatedComponents = await Promise.all(
+            originalComponents.map(async (component, index) => {
+              // Create a new component object for this translation
+              const translatedComponent = { ...component };
+              
+              try {
+                // Translate title
+                this.logger.debug(`Translating component ${index + 1}/${originalComponents.length} title`);
+                translatedComponent.title = await this.translatorService.translate(
+                  component.title,
+                  sourceLang,
+                  targetLang
+                );
+                
+                // Translate options if they exist
+                if (component.options?.length) {
+                  this.logger.debug(`Translating ${component.options.length} options for component ${index + 1}`);
+                  const translatedOptions = await Promise.all(
+                    component.options.map(async (option, optionIndex) => {
+                      const translated = await this.translatorService.translate(
+                        option,
+                        sourceLang,
+                        targetLang
+                      );
+                      this.logger.debug(`Translated option ${optionIndex + 1}/${component.options.length} for component ${index + 1}`);
+                      return translated;
+                    })
+                  );
+                  translatedComponent.options = translatedOptions;
+                }
+                
+                this.logger.debug(`Completed component ${index + 1}/${originalComponents.length}`);
+                return translatedComponent;
+              } catch (error) {
+                this.logger.error(`Failed to translate component ${index + 1}`, {
+                  error,
+                  component: component.title
+                });
+                throw error;
+              }
+            })
+          );
 
-          this.logger.log('Components translated', { 
-            targetLang,
+          this.logger.log(`All components translated for ${surveyId} to ${targetLang}`, { 
             componentCount: translatedComponents.length 
           });
 
@@ -74,7 +96,7 @@ export class TranslateService implements OnModuleInit {
           } as any;
 
           await this.surveyService.updateSurvey(surveyId, updatedSurvey);
-          this.logger.log(`Completed translation for survey ${surveyId} to ${targetLang}`);
+          this.logger.log(`Successfully updated survey ${surveyId} with ${targetLang} translations`);
         } catch (error) {
           this.logger.error(`Failed to translate survey ${surveyId} to ${targetLang}`, error);
           throw error;
